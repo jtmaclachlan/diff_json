6677384a9105423a9bfffdd252534ff436c1df9e
Josh MacLachlan <josh.t.maclachlan@gmail.com>
2019-08-07 08:07:57 -0400

    Bootstrap div output

--- lib/diff_json/output/html_output.rb
+++ lib/diff_json/output/html_output.rb
@@ -3,7 +3,8 @@ module DiffJson
     def initialize(diff, **opts)
       @diff = diff
       @opts = {
-        table_id_prefix: 'diff_json_view_0'
+        table_id_prefix: 'diff_json_view_0',
+        markup_type: :bootstrap
       }
       @markup = build
     end
@@ -29,11 +30,12 @@ module DiffJson
     end
 
     def table_markup(table_diff, table_id_prefix)
-      markup = {left: "", right: "", full: "", sub_diffs: {}}
+      markup_lines = {left: "", right: "", full: "", sub_diffs: {}}
 
-      markup[:left]  = "<table id=\"#{table_id_prefix}_left\" class=\"diff-json-view diff-json-split-view-left\">\n"
-      markup[:right] = "<table id=\"#{table_id_prefix}_right\" class=\"diff-json-view diff-json-split-view-right\">\n"
-      markup[:full]  = "<table id=\"#{table_id_prefix}_full\" class=\"diff-json-view diff-json-full-view\">\n"
+      html_opener = self.method("html_#{@opts[:markup_type]}_opener".to_sym).call(table_id_prefix)
+      markup_lines[:left]  = html_opener[:left]
+      markup_lines[:right] = html_opener[:right]
+      markup_lines[:full]  = html_opener[:full]
 
       hierarchy_lock = nil
       structure_queue = []
@@ -81,10 +83,10 @@ module DiffJson
             hierarchy_lock = path unless old_element[:type] == :primitive
           end
 
-          compiled_lines = html_lines(left_operators, old_lines, right_operators, new_lines)
-          markup[:left] << compiled_lines[:left]
-          markup[:right] << compiled_lines[:right]
-          markup[:full] << compiled_lines[:full]
+          compiled_lines = self.method("html_#{@opts[:markup_type]}_lines".to_sym).call(left_operators, old_lines, right_operators, new_lines)
+          markup_lines[:left] << compiled_lines[:left]
+          markup_lines[:right] << compiled_lines[:right]
+          markup_lines[:full] << compiled_lines[:full]
         end
 
         unless structure_queue.empty?
@@ -92,59 +94,129 @@ module DiffJson
             structure_queue.reverse.each do |sq|
               old_lines = jpg(nil, structure: true, structure_position: :close, structure_type: sq[:type], indentation: sq[:indentation], trailing_comma: sq[:old_comma])
               new_lines = jpg(nil, structure: true, structure_position: :close, structure_type: sq[:type], indentation: sq[:indentation], trailing_comma: sq[:new_comma])
-              compiled_lines = html_lines('  ', old_lines, '  ', new_lines)
-              markup[:left] << compiled_lines[:left]
-              markup[:right] << compiled_lines[:right]
-              markup[:full] << compiled_lines[:full]
+              compiled_lines = self.method("html_#{@opts[:markup_type]}_lines".to_sym).call('  ', old_lines, '  ', new_lines)
+              markup_lines[:left] << compiled_lines[:left]
+              markup_lines[:right] << compiled_lines[:right]
+              markup_lines[:full] << compiled_lines[:full]
             end
           else
             if (table_diff.paths[(i+1)] =~ /^#{structure_queue.last[:path]}.+$/).nil?
               sq = structure_queue.pop
               old_lines = jpg(nil, structure: true, structure_position: :close, structure_type: sq[:type], indentation: sq[:indentation], trailing_comma: sq[:old_comma])
               new_lines = jpg(nil, structure: true, structure_position: :close, structure_type: sq[:type], indentation: sq[:indentation], trailing_comma: sq[:new_comma])
-              compiled_lines = html_lines('  ', old_lines, '  ', new_lines)
-              markup[:left] << compiled_lines[:left]
-              markup[:right] << compiled_lines[:right]
-              markup[:full] << compiled_lines[:full]
+              compiled_lines = self.method("html_#{@opts[:markup_type]}_lines".to_sym).call('  ', old_lines, '  ', new_lines)
+              markup_lines[:left] << compiled_lines[:left]
+              markup_lines[:right] << compiled_lines[:right]
+              markup_lines[:full] << compiled_lines[:full]
             end
           end
         end
       end
 
-      markup[:left] += "</table>"
-      markup[:right] += "</table>"
-      markup[:full] += "</table>"
+      html_closer = self.method("html_#{@opts[:markup_type]}_closer".to_sym).call
+      markup_lines[:left] << html_closer[:left]
+      markup_lines[:right] << html_closer[:right]
+      markup_lines[:full] << html_closer[:full]
 
-      return markup
+      return markup_lines
     end
 
-    def html_lines(left_operators, left_lines, right_operators, right_lines)
+    def html_table_opener(table_id_prefix)
+      compiled_lines = {}
+      compiled_lines[:left]  = "<table id=\"#{table_id_prefix}_left\" class=\"diff-json-view diff-json-split-view-left\">\n"
+      compiled_lines[:right] = "<table id=\"#{table_id_prefix}_right\" class=\"diff-json-view diff-json-split-view-right\">\n"
+      compiled_lines[:full]  = "<table id=\"#{table_id_prefix}_full\" class=\"diff-json-view diff-json-full-view\">\n"
+
+      return compiled_lines
+    end
+
+    def html_table_lines(left_operators, left_lines, right_operators, right_lines)
       compiled_lines = {left: "", right: "", full: ""}
+
       (0..(left_lines.length - 1)).each do |i|
         compiled_lines[:left] << <<-EOL
           <tr class="diff-json-view-line">
-            <td class="diff-json-view-line-operator"><pre>#{left_operators}</pre></td>
+            <td class="diff-json-view-line-operator"><pre>#{left_operators unless left_lines[i].empty?}</pre></td>
             <td class="diff-json-view-line-content"><pre class="diff-json-line-breaker">#{left_lines[i]}</pre></td>
           </tr>
         EOL
         compiled_lines[:right] << <<-EOL
         <tr class="diff-json-view-line">
-          <td class="diff-json-view-line-operator"><pre>#{right_operators}</pre></td>
+          <td class="diff-json-view-line-operator"><pre>#{right_operators unless right_lines[i].empty?}</pre></td>
           <td class="diff-json-view-line-content"><pre class="diff-json-line-breaker">#{right_lines[i]}</pre></td>
         </tr>
         EOL
         compiled_lines[:full] << <<-EOL
         <tr class="diff-json-view-line">
           <div class="row">
-            <td class="diff-json-view-line-operator"><pre>#{left_operators}</pre></td>
+            <td class="diff-json-view-line-operator"><pre>#{left_operators unless left_lines[i].empty?}</pre></td>
             <td class="diff-json-view-line-content"><pre class="diff-json-line-breaker">#{left_lines[i]}</pre></td>
             <td class="diff-json-view-column-break"></td>
-            <td class="diff-json-view-line-operator"><pre>#{right_operators}</pre></td>
+            <td class="diff-json-view-line-operator"><pre>#{right_operators unless right_lines[i].empty?}</pre></td>
             <td class="diff-json-view-line-content"><pre class="diff-json-line-breaker">#{right_lines[i]}</pre></td>
           </div>
         </tr>
         EOL
       end
+
+      return compiled_lines
+    end
+
+    def html_table_closer
+      compiled_lines = {}
+      compiled_lines[:left] = "</table>"
+      compiled_lines[:right] = "</table>"
+      compiled_lines[:full] = "</table>"
+
+      return compiled_lines
+    end
+
+    def html_bootstrap_opener(table_id_prefix)
+      compiled_lines = {}
+      compiled_lines[:left]  = "<div id=\"#{table_id_prefix}_left\" class=\"diff-json-view diff-json-split-view-left col-xs-6 col-6\">\n"
+      compiled_lines[:right] = "<div id=\"#{table_id_prefix}_right\" class=\"diff-json-view diff-json-split-view-right col-xs-6 col-6\">\n"
+      compiled_lines[:full]  = "<div id=\"#{table_id_prefix}_full\" class=\"diff-json-view diff-json-full-view col-xs-12 col-12\">\n"
+
+      return compiled_lines
+    end
+
+    def html_bootstrap_lines(left_operators, left_lines, right_operators, right_lines)
+      compiled_lines = {left: "", right: "", full: ""}
+
+      (0..(left_lines.length - 1)).each do |i|
+        compiled_lines[:left] << <<-EOL
+          <div class="diff-json-view-line row">
+            <div class="diff-json-view-line-operator col-1"><pre>#{left_operators unless left_lines[i].empty?}</pre></div>
+            <div class="diff-json-view-line-content col-11"><pre class="diff-json-line-breaker">#{left_lines[i]}</pre></div>
+          </div>
+        EOL
+        compiled_lines[:right] << <<-EOL
+        <div class="diff-json-view-line row">
+          <div class="diff-json-view-line-operator col-1"><pre>#{right_operators unless right_lines[i].empty?}</pre></div>
+          <div class="diff-json-view-line-content col-11"><pre class="diff-json-line-breaker">#{right_lines[i]}</pre></div>
+        </div>
+        EOL
+        compiled_lines[:full] << <<-EOL
+        <div class="diff-json-view-line row">
+          <div class="diff-json-view-line-left col-6">
+            <pre class="diff-json-line-breaker">#{left_operators unless left_lines[i].empty?} #{left_lines[i]}</pre>
+          </div>
+          <div class="diff-json-view-line-right col-6">
+            <pre class="diff-json-line-breaker">#{right_operators unless right_lines[i].empty?} #{right_lines[i]}</pre>
+          </div>
+        </div>
+        EOL
+      end
+
+      return compiled_lines
+    end
+
+    def html_bootstrap_closer
+      compiled_lines = {}
+      compiled_lines[:left] = "</div>"
+      compiled_lines[:right] = "</div>"
+      compiled_lines[:full] = "</div>"
+
       return compiled_lines
     end
 
